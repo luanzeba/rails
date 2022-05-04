53e17ed0a5e70bbb9c0353c1bfa0cf89174fbce3
Luan Vieira <luanzeba@github.com>
2022-05-04 15:11:01 -0700

    Remove :primary_key declaration from associations
    All of the associations for `DestroyAsyncParent` used to explicitly
    declare a primary key. Those are not be necessary since Rails can
    figure out the the primary keys for the associations without manually
    declaring them.
    Moreover, manually including the primary keys ended up
    causing the association queries to be incorrect.
    
    Co-authored-by: Daniel Colson <composerinteralia@github.com>

--- activerecord/test/models/destroy_async_parent.rb
+++ activerecord/test/models/destroy_async_parent.rb
@@ -4,12 +4,12 @@
    self.primary_key = "parent_id"
 
    has_one :dl_keyed_has_one, dependent: :destroy_async,
-     foreign_key: :destroy_async_parent_id, primary_key: :parent_id
+     foreign_key: :destroy_async_parent_id
    has_many :dl_keyed_has_many, dependent: :destroy_async,
-     foreign_key: :many_key, primary_key: :parent_id
+     foreign_key: :many_key
    has_many :dl_keyed_join, dependent: :destroy_async,
-     foreign_key: :destroy_async_parent_id, primary_key: :joins_key
+     foreign_key: :destroy_async_parent_id
    has_many :dl_keyed_has_many_through,
      through: :dl_keyed_join, dependent: :destroy_async,
-     foreign_key: :dl_has_many_through_key_id, primary_key: :through_key
+     foreign_key: :dl_has_many_through_key_id
  end
